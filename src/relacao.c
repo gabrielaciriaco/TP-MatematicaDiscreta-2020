b7bd45fb4dba6e0c1cb44c1fd9747e83bdf21080
Gabriela Ciriaco <gabs.ciriaco@gmail.com>
2020-08-04 13:10:09 -0300

    Leitura do arquivo

--- src/relacao.c
+++ src/relacao.c
@@ -7,15 +7,42 @@ typedef struct{
 }ParOrdenado;
 
 int main(){
+    //Entrada de dados
+    FILE * arquivo;
+    arquivo = fopen("entrada.txt","r");
     int quantidadeDeElementos;
     int *elementos;
-    
-    scanf("%d ",&quantidadeDeElementos);
-    elementos = (int*) malloc(quantidadeDeElementos*sizeof(int));
+    char linha[5];
+    int teste1;
+    int teste2;
+    if(arquivo != NULL){
+        int contador = 0;
+        while(!feof(arquivo)){
+            if(contador == 0){
+                fscanf(arquivo,"%d",&quantidadeDeElementos);
+                elementos = (int*) malloc(quantidadeDeElementos*sizeof(int));
+                for(int i = 0; i< quantidadeDeElementos; i++){
+                    fscanf(arquivo,"%d",&elementos[i]);
+                    printf("Elemento %d: %d\n",i,elementos[i]);
+                }
+            }else{
+                fscanf(arquivo,"%d",&teste1);
+                fscanf(arquivo,"%d",&teste2);
+                printf("%d %d\n",teste1,teste2);
+                //Todo armazenar par ordenado
+            }
+            fgets(linha,sizeof(linha),arquivo);
+            contador++;
+        }
+        printf("Quantidade de elementos: %d ",quantidadeDeElementos);
 
-    for (int i=0; i<quantidadeDeElementos; i++){
-        scanf("%d", &(elementos[i]));
+        //Todo Comparacoes
+
+        //Todo: saida
+        return 0;
+
+    }else{
+        printf("Erro, não foi possível abrir o arquivo");
+        return 0;
     }
-
-    return 0;
 }
