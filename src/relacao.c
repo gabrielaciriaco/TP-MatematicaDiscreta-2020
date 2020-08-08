0acdf039e69efc45db43672b67e187697fd79aad
Gabriela Ciriaco <gabs.ciriaco@gmail.com>
2020-08-08 15:48:08 -0300

    leitura do arquivo e extração dos elementos do array de elementos e pares ordenados

--- src/relacao.c
+++ src/relacao.c
@@ -1,48 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_TAMANHO 10000
 
-typedef struct{
+typedef struct
+{
     int x;
     int y;
-}ParOrdenado;
+} ParOrdenado;
 
-int main(){
-    //Entrada de dados
-    FILE * arquivo;
-    arquivo = fopen("entrada.txt","r");
+int main()
+{
+    FILE *arquivo;
+    arquivo = fopen("entrada.txt", "r");
+
     int quantidadeDeElementos;
     int *elementos;
-    char linha[5];
-    int teste1;
-    int teste2;
-    if(arquivo != NULL){
-        int contador = 0;
-        while(!feof(arquivo)){
-            if(contador == 0){
-                fscanf(arquivo,"%d",&quantidadeDeElementos);
-                elementos = (int*) malloc(quantidadeDeElementos*sizeof(int));
-                for(int i = 0; i< quantidadeDeElementos; i++){
-                    fscanf(arquivo,"%d",&elementos[i]);
-                    printf("Elemento %d: %d\n",i,elementos[i]);
-                }
-            }else{
-                fscanf(arquivo,"%d",&teste1);
-                fscanf(arquivo,"%d",&teste2);
-                printf("%d %d\n",teste1,teste2);
-                //Todo armazenar par ordenado
-            }
-            fgets(linha,sizeof(linha),arquivo);
-            contador++;
+    int quantidadeParesOrdenados = 0;
+    ParOrdenado paresOrdenados[MAX_TAMANHO];
+
+    if (arquivo != NULL)
+    {
+        char linha[MAX_TAMANHO];
+
+        fscanf(arquivo, "%d", &quantidadeDeElementos);
+        elementos = (int *)malloc(quantidadeDeElementos * sizeof(int));
+        printf("Quantidade de elementos: %d\n", quantidadeDeElementos); //retirar
+
+        for (int i = 0; i < quantidadeDeElementos; i++)
+        {
+            fscanf(arquivo, "%d", &elementos[i]);
+            printf("Elemento %d: %d\n", i, elementos[i]);
+        };
+
+        while (!feof(arquivo) && quantidadeParesOrdenados<MAX_TAMANHO)
+        {
+            int cordenadaX,cordenadaY;
+            fscanf(arquivo, "%d", &cordenadaX);
+            fscanf(arquivo, "%d", &cordenadaY);
+            paresOrdenados[quantidadeParesOrdenados].x = cordenadaX;
+            paresOrdenados[quantidadeParesOrdenados].y = cordenadaY;
+            printf("Cordenada %d x:%d\n",quantidadeParesOrdenados,paresOrdenados[quantidadeParesOrdenados].x); //retirar
+            printf("Cordenada %d y:%d\n\n",quantidadeParesOrdenados,paresOrdenados[quantidadeParesOrdenados].y); //retirar
+            quantidadeParesOrdenados++;
         }
-        printf("Quantidade de elementos: %d ",quantidadeDeElementos);
-
-        //Todo Comparacoes
-
-        //Todo: saida
-        return 0;
-
-    }else{
-        printf("Erro, não foi possível abrir o arquivo");
+        fclose(arquivo);
+        printf("quantidade: %d",quantidadeParesOrdenados); //retirar
+    }
+    else
+    {
+        printf("Erro, não foi possível abrir o arquivo"); 
         return 0;
     }
 }
