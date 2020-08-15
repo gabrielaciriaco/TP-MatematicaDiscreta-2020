6b8b965afa59be8f309c6544c07de290b5b8266c
Gabriela Ciriaco <gabs.ciriaco@gmail.com>
2020-08-15 17:21:08 -0300

    relações simétrica, anti-simétrica e assimétrica

--- src/relacao.c
+++ src/relacao.c
@@ -97,7 +97,7 @@ int main()
         {
             if (matrizAdjacencias[i][i] != 1)
             {
-                printf("(%d,%d)", elementos[i], elementos[i]);
+                printf("(%d,%d); ", elementos[i], elementos[i]);
             }
         }
         printf("\n");
@@ -125,45 +125,131 @@ int main()
         {
             if (matrizAdjacencias[i][i] != 0)
             {
-                printf("(%d,%d)", elementos[i], elementos[i]);
+                printf("(%d,%d); ", elementos[i], elementos[i]);
             }
         }
         printf("\n");
     }
 
     //verificando a relação simetrica
+    printf("2. Simétrica: ");
+    ParOrdenado faltaParaSerSimetrica[quantidadeParesOrdenados];
+    int EhSimetrica = 1;
+    int numeroDeFaltaParaSerSimetrica = 0;
+    for (int i = 0; i < quantidadeDeElementos; i++)
+    {
+        for (int j = 0; j < quantidadeDeElementos; j++)
+        {
+            if (matrizAdjacencias[i][j] == 1)
+            {
+                if (matrizAdjacencias[i][j] != matrizAdjacencias[j][i])
+                {
+                    EhSimetrica = 0;
+                    faltaParaSerSimetrica[numeroDeFaltaParaSerSimetrica].x = elementos[i];
+                    faltaParaSerSimetrica[numeroDeFaltaParaSerSimetrica].y = elementos[j];
+                    numeroDeFaltaParaSerSimetrica++;
+                }
+            }
+        }
+    }
+    if (EhSimetrica == 1)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n");
+        printf("   ");
+        for (int i = 0; i < numeroDeFaltaParaSerSimetrica; i++)
+        {
+            printf("(%d,%d); ", faltaParaSerSimetrica[i].x, faltaParaSerSimetrica[i].y);
+        }
+        printf("\n");
+    }
 
-    // printf("2. Simétrica: ");
-    // ParOrdenado faltaParaSerSimetrica[quantidadeParesOrdenados];
-    // int EhSimetrica = 1;
-    // int numeroDeFaltaParaSerSimetrica = 0;
-    // for (int i = 0; i < quantidadeDeElementos; i++)
-    // {
-    //     for (int j = 0; j < quantidadeDeElementos; j++)
-    //     {
-    //         if (matrizAdjacencias[i][j] != matrizAdjacencias[j][i])
-    //         {
-    //             EhSimetrica = 0;
-    //             faltaParaSerSimetrica[numeroDeFaltaParaSerSimetrica].x = elementos[i];
-    //             faltaParaSerSimetrica[numeroDeFaltaParaSerSimetrica].y = elementos[j];
-    //             numeroDeFaltaParaSerSimetrica++;
-    //         }
-    //     }
-    // }
-    // if (EhSimetrica)
-    // {
-    //     printf("V\n");
-    // }
-    // else
-    // {
-    //     printf("F\n");
-    //     printf("   ");
-    //     for (int i = 0; i < numeroDeFaltaParaSerSimetrica; i++)
-    //     {
-    //             printf("(%d,%d)", faltaParaSerSimetrica[i].x, faltaParaSerSimetrica[i].y);
-    //     }
-    //     printf("\n");
-    // }
+    //verificando a relacao anti simetrica
+    printf("2. Anti-simétrica: ");
+    ParOrdenado faltaParaSerAntiSimetrica[quantidadeParesOrdenados];
+    int EhAntiSimetrica = 1;
+    int numeroDeFaltaParaSerAntiSimetrica = 0;
+    int jaEstaNoVetor = 0;
+    for (int i = 0; i < quantidadeDeElementos; i++)
+    {
+        for (int j = 0; j < quantidadeDeElementos; j++)
+        {
+            if (matrizAdjacencias[i][j] == 1)
+            {
+                if (matrizAdjacencias[i][j] == matrizAdjacencias[j][i] && i != j)
+                {
+                    for (int x = 0; x < numeroDeFaltaParaSerAntiSimetrica; x++)
+                    {
+                        if (faltaParaSerAntiSimetrica[x].y == elementos[i] && faltaParaSerAntiSimetrica[x].x == elementos[j])
+                        {
+                            jaEstaNoVetor = 1;
+                        }
+                    }
+                    if (!jaEstaNoVetor)
+                    {
+                        EhAntiSimetrica = 0;
+                        faltaParaSerAntiSimetrica[numeroDeFaltaParaSerAntiSimetrica].x = elementos[i];
+                        faltaParaSerAntiSimetrica[numeroDeFaltaParaSerAntiSimetrica].y = elementos[j];
+                        numeroDeFaltaParaSerAntiSimetrica++;
+                    }
+                    jaEstaNoVetor = 0;
+                }
+            }
+        }
+    }
+    if (EhAntiSimetrica == 1)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n");
+        printf("   ");
+        for (int i = 0; i < numeroDeFaltaParaSerAntiSimetrica; i++)
+        {
+            printf("(%d,%d) e (%d,%d); ", faltaParaSerAntiSimetrica[i].x, faltaParaSerAntiSimetrica[i].y, faltaParaSerAntiSimetrica[i].y, faltaParaSerAntiSimetrica[i].x);
+        }
+        printf("\n");
+    }
+
+    //verificando a relacao assimética
+    printf("2. Assimétrica: ");
+    ParOrdenado faltaParaSerAssimetrica[quantidadeParesOrdenados];
+    int EhAssimetrica = 1;
+    int numeroDeFaltaParaSerAssimetrica = 0;
+    for (int i = 0; i < quantidadeDeElementos; i++)
+    {
+        for (int j = 0; j < quantidadeDeElementos; j++)
+        {
+            if (matrizAdjacencias[i][j] == 1)
+            {
+                if (matrizAdjacencias[i][j] == matrizAdjacencias[j][i])
+                {
+                    EhAssimetrica = 0;
+                    faltaParaSerAssimetrica[numeroDeFaltaParaSerAssimetrica].x = elementos[i];
+                    faltaParaSerAssimetrica[numeroDeFaltaParaSerAssimetrica].y = elementos[j];
+                    numeroDeFaltaParaSerAssimetrica++;
+                }
+            }
+        }
+    }
+    if (EhAssimetrica == 1)
+    {
+        printf("V\n");
+    }
+    else
+    {
+        printf("F\n");
+        printf("   ");
+        for (int i = 0; i < numeroDeFaltaParaSerAssimetrica; i++)
+        {
+            printf("(%d,%d); ", faltaParaSerAssimetrica[i].x, faltaParaSerAssimetrica[i].y);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
